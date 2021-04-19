b607975ec8add2c6118c4ca6d320a69c5313f373
DEEPAK SINGH <58417519+deepak-singh-au6@users.noreply.github.com>
2021-04-19 12:55:34 +0530

    16-04-2021

--- 13-april-2021/server.js
+++ 13-april-2021/server.js
@@ -55,10 +55,53 @@ app.post('/addTodo', (req, res) => {
 
 })
 
-//server will send something to clinet
-app.get('/health', (req, res) => {
-    res.send("its working")
+// returning all todos
+app.get('/getTodos', (request, response) => {
+    response.status(200).json({success:true, message:`${todos.length} todos found successfuly`,result: todos})
+    
 })
+
+//get single todo
+
+app.get('/getSingleTodo/:id', (req, res) => {
+    // param is an object
+    const id = parseInt(req.params.id)
+    const todo = todos.find(obj => obj.id === id)
+    if (todo) {
+        return res.status(200).json({message:"Todos found successfully", success: true,result: todo})
+    }
+    return res.status(404).json({message:"Not found", success:false, result:{}})
+})
+
+// To update todo
+app.put('/updateTodo/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const todo = req.body
+    const index = todos.findIndex(obj => obj.id === id)
+    if (index !== -1) {
+        todos[index] = todo
+        return res.status(200).json({message:"Successfully updated", success:true, result: todos[index]})
+    }
+    return res.status(404).json({ message: "Not found", success: false, result: {} })
+})
+
+
+//delete a todo
+
+app.delete('/deleteTodo/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const index = todos.findIndex(obj => obj.id === id)
+    if (index !== -1) {
+        todos.slice[index, 1]
+        return res.status(200).json({message:"successfully deleted", success:true})
+    }
+    return res.status(404).json({message:'Not found', success:false})
+})
+
+//server will send something to clinet
+// app.get('/health', (req, res) => {
+//     res.send("its working")
+// })
 
 const PORT = 5555
 app.listen(PORT, () => {
